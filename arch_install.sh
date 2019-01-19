97fdd61e35c92902738661dc2a1cdbd8ed2c6734
Aleš Mrázek <mrazek.ales67@gmail.com>
2019-01-19 16:27:57 +0100

    Create arch_install.sh

---
+++ arch_install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Arch Linux Installer
+# --------------------------------
+# author    : Mrazek Ales
+#             https://github.com/mrazekales
+#
+# project   : https://github.com/mrazekales/myarch-desktop
+#
+# license   : LGPL-3.0 (http://opensource.org/licenses/lgpl-3.0.html)
+
